8055d795e5045636d8856e1e12b265e39558ced6
cris <cris161729@gmail.com>
2026-04-15 18:57:26 -0600

    refactor: simplifica main de presentacion

--- src/presentacion/Main.java
+++ src/presentacion/Main.java
@@ -1,24 +1,16 @@
 package presentacion;
 
 import javax.swing.SwingUtilities;
-import javax.swing.UIManager;
 
 public class Main {
 
     public static void main(String[] args) {
-        aplicarEstiloDelSistema();
-
-        SwingUtilities.invokeLater(() -> {
-            VentanaPrincipal ventanaPrincipal = new VentanaPrincipal();
-            ventanaPrincipal.setVisible(true);
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                VentanaPrincipal ventanaPrincipal = new VentanaPrincipal();
+                ventanaPrincipal.setVisible(true);
+            }
         });
-    }
-
-    private static void aplicarEstiloDelSistema() {
-        try {
-            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-        } catch (Exception ex) {
-            // Si falla el estilo del sistema, Swing usa el estilo por defecto.
-        }
     }
 }
